0c56f193dca967a67638951d641a278344b6e38a
prakashranjan <prakashranjansingh04@gmail.com>
2018-05-12 20:21:09 +0530

    update with GUI and pie chart

--- a_fier.py
+++ a_fier.py
@@ -9,13 +9,18 @@ import spotipy
 import webbrowser
 import spotipy.util as util
 from json.decoder import JSONDecodeError
+import urllib.parse
+from sklearn.metrics import confusion_matrix
+
+
 
 
 plt.style.use('ggplot') # make plots look better
 
 
 #### import the data ####
-df = pd.read_csv("finaldset.csv")
+#df = pd.read_csv("finaldset.csv")
+df = pd.read_csv("f1combfinal2.csv")
 #df = pd.read_csv("lyset31_g.csv")
 
 #print (df.head())
@@ -23,10 +28,10 @@ print (df.describe())
 
 
 #### Manual Feature selection ####
-sns.FacetGrid(df,
-	hue="mood").map(plt.scatter, "valence", "energy").add_legend()
+#sns.FacetGrid(df,
+	#hue="mood").map(plt.scatter, "valence", "energy").add_legend()
 
-plt.show() # expect to see three defined clusters
+#plt.show() # expect to see three defined clusters
 # it's seen that the petal features seem to describe the species the best
 
 
@@ -55,7 +60,7 @@ features = vec.fit_transform(df_features).toarray()
 ##### split up in test and training data ####
 from sklearn.model_selection import train_test_split
 
-features_train, features_test, labels_train, labels_test = train_test_split( features, labels, test_size=0.20, random_state=42)
+features_train, features_test, labels_train, labels_test = train_test_split( features, labels, test_size=0.20, random_state=91)
 
 
 #### Fit to random forests ####
@@ -80,7 +85,7 @@ clf.fit(features_train, labels_train)
 # find the accuracy of the model
 acc_test = clf.score(features_test, labels_test)
 acc_train = clf.score(features_train, labels_train)
-print ("Train Accuracy:", acc_train)
+#print ("Train Accuracy:", acc_train)
 print ("Test Accuracy:", acc_test)
 print("---------------------------------------------")
 #acc_test2 = clf2.score(features_test, labels_test)
@@ -94,23 +99,23 @@ print("---------------------------------------------")
 
 # compute predictions on test features
 pred = clf.predict(features_test)
-'''
-# predict our new unique iris flower
-flower = [[5.2,0.9]]
-class_code = clf.predict(flower) # [1]
 
-decoded_class = le.inverse_transform(class_code)
-print (decoded_class) # ['Iris-versicolor']
-
-'''
 #### Figure out what kind of mistakes it makes ####
 from sklearn.metrics import recall_score, precision_score
 
-#precision = precision_score(labels_test, pred, average="weighted")
-#recall = recall_score(labels_test, pred, average="weighted")
+precision = precision_score(labels_test, pred, average="weighted")
+recall = recall_score(labels_test, pred, average="weighted")
 
-#print ("Precision:", precision)
-#print ("Recall:", recall)
+print ("Precision:", precision)
+print ("Recall:", recall)
+
+print("confusion matrix")
+cm = confusion_matrix(labels_test, pred)
+print(cm)
+plt.matshow(cm)
+plt.title('Confusion matrix of the acoustic classifier')
+plt.colorbar()
+plt.show()
 
 #code for new prediction...
 
@@ -135,60 +140,124 @@ token = util.prompt_for_user_token(username=username, client_id=CLIENT_ID, clien
 #creating sptifyObject
 #user= spoto.current_user()
 #print(json.dumps(user,sort_keys=True, indent=4))
-
+from tkinter import *
 if token:
-    aname = input('Artist name-> ')
-    titlen = input('title-> ')
-    spoto = spotipy.Spotify(auth=token)
-    q = "artist:{} track:{}".format(aname, titlen)
-    #print(q)
-    try:
-        track_find = spoto.search(q, limit=1, offset=0, type='track', market=None)
-        track_id = track_find['tracks']['items'][0]['id']
-        acou_data = spoto.audio_features(track_id)
-        tempo = acou_data[0]['tempo']
-        print("tempo : ", tempo)
-        energy = acou_data[0]['energy']
-        print("energy : ", energy)
-        loudness = acou_data[0]['loudness']
-        print("loudness : ", loudness)
-        danceability = acou_data[0]['danceability']
-        print("danceability : ", danceability)
-        valence = acou_data[0]['valence']
-        print("valence : ", valence)
-        acousticness = acou_data[0]['acousticness']
-        print("acousticness : ", acousticness)
-        tr_json = acou_data[0]
-        tr_json_d = json.dumps(tr_json, sort_keys=True, indent=1)
+    def predict_song(aname, titlen, clf):
+        # aname = input('Artist name-> ')
+        # titlen = input('title-> ')
+        if aname=="" or titlen=="":
+            print("not music meta found---------------------------")
+            res.configure(text=" No Music meta found")
+            return
+        spoto = spotipy.Spotify(auth=token)
+        q = "artist:{} track:{}".format(aname, titlen)
+        # print(q)
+        try:
+            track_find = spoto.search(q, limit=1, offset=0, type='track', market=None)
+            track_id = track_find['tracks']['items'][0]['id']
+            acou_data = spoto.audio_features(track_id)
+            tempo = acou_data[0]['tempo']
+            print("tempo : ", tempo)
+            energy = acou_data[0]['energy']
+            print("energy : ", energy)
+            loudness = acou_data[0]['loudness']
+            print("loudness : ", loudness)
+            danceability = acou_data[0]['danceability']
+            print("danceability : ", danceability)
+            valence = acou_data[0]['valence']
+            print("valence : ", valence)
+            acousticness = acou_data[0]['acousticness']
+            print("acousticness : ", acousticness)
+            tr_json = acou_data[0]
+            tr_json_d = json.dumps(tr_json, sort_keys=True, indent=1)
 
-    except:
-        track_id=''
-    if track_id:
-        #print("\ntrack_id-->", track_id)
-        sr_json=json.dumps(track_find, sort_keys=True, indent=1)
-        music = [[tempo, energy, danceability, loudness, valence, acousticness]]
-        class_code = clf.predict(music)
-        # print(class_code)
-        decoded_class = le.inverse_transform(class_code)
-        # print (decoded_class)
-        if decoded_class == 0:
-            print("class-> happy ")
-        elif decoded_class == 1:
-            print("class-> angry ")
-        elif decoded_class == 2:
-            print("class-> sad ")
-        elif decoded_class == 3:
-            print("class-> relax ")
+        except:
+            track_id = ''
+        if track_id:
+            # print("\ntrack_id-->", track_id)
+            sr_json = json.dumps(track_find, sort_keys=True, indent=1)
+            music = [[tempo, energy, danceability, loudness, valence, acousticness]]
+            class_code = clf.predict(music)
+            confi = clf.predict_proba(music)
+            print("\n-------------------------------------------------------------------------------------------\n")
+            ha = confi[0][0] * 100
+            print("happy %->", ha)
+            an = confi[0][1] * 100
+            print("angry %->", an)
+            sa = confi[0][2] * 100
+            print("sad %->", sa)
+            rel = confi[0][3] * 100
+            print("relax %->", rel)
+            # Pie chart, where the slices will be ordered and plotted counter-clockwise:
+            labels = 'Happy', 'Angry', 'Sad', 'Relax'
+            sizes = [ha, an, sa, rel]
+            elevate = sizes.index(max(sizes))
+            explode = (0, 0, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+            explode = list(explode)
+            explode[elevate] = 0.1
+            explode = tuple(explode)
+            fig1, ax1 = plt.subplots()
+            ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+                    shadow=True, startangle=90)
+            ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+
+            print("----------------------------------------------------------------------------------------------")
+            # print(class_code)
+            decoded_class = le.inverse_transform(class_code)
+            # print (decoded_class)
+            if decoded_class == 0:
+                print("class-> happy ")
+                res.configure(text="Happy")
+            elif decoded_class == 1:
+                print("class-> angry ")
+                res.configure(text="Angry")
+            elif decoded_class == 2:
+                print("class-> sad ")
+                res.configure(text="Sad")
+            elif decoded_class == 3:
+                print("class-> relax ")
+                res.configure(text="Relax")
+            else:
+                print("failure state")
+            fr = {'search_query': str(titlen) + " " + str(aname)}
+            encoded = urllib.parse.urlencode(fr)
+            print("\n youtube---> https://www.youtube.com/results?" + str(encoded))
+            plt.show()
+            print("\n")
+
         else:
-            print("failure state")
-    else:
-        print("not music meta found---------------------------")
+            print("not music meta found---------------------------")
+            res.configure(text="No Music meta found")
+            # theta = int(input("press 1 to try next song--> "))
 
 
+    def show_entry_fields():
+        #print("First Name: %s\nLast Name: %s" % (e1.get(), e2.get()))
+        predict_song(e1.get(), e2.get(),clf)
+
+
+    master = Tk()
+    Label(master, text="Artist Name").grid(row=0, pady=10)
+    Label(master, text="Song Title").grid(row=1, pady=10)
+
+    e1 = Entry(master)
+    e2 = Entry(master)
+
+
+    e1.grid(row=0, column=1)
+    e2.grid(row=1, column=1)
+
+    Button(master, text='Quit', command=master.quit).grid(row=3, column=0, sticky=W, pady=20)
+    Button(master, text='Classify', command=show_entry_fields).grid(row=3, column=1, sticky=W, pady=20)
+
+    Label(master, text="------ Mood Classifier Acoustic ------" ,bg="yellow", fg="black").grid(row=5, pady=10)
+    res = Label(master ,fg="blue")
+    res.grid(row=6 ,pady=20)
+    mainloop()
 
 else:
     print("not working token-------------")
-
 
 
 
--- t_fier_new.py
+++ t_fier_new.py
@@ -12,18 +12,42 @@ from PyLyrics import *
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import confusion_matrix
 from sklearn.linear_model import LogisticRegression
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
+import urllib.parse
 
+
+def wcloud(word_string):
+    # Convert all the required text into a single string here
+    # and store them in word_string
+
+    # you can specify fonts, stopwords, background color and other options
+    wordcloud = WordCloud(
+        stopwords=STOPWORDS,
+        background_color='white',
+        width=1200,
+        height=1000
+    ).generate(word_string)
+
+    plt.imshow(wordcloud)
+    plt.axis('off')
+    plt.show()
 
 #data = pd.read_csv("finaldset.csv")
 #data = pd.read_csv("lyset31_g.csv")
-data = pd.read_csv("f1comb.csv")
+data = pd.read_csv("f1combfinal2.csv")
+
+#print("0-> ", len(data[data.mood==0]))
+#print("1-> ", len(data[data.mood==1]))
+#print("2-> ", len(data[data.mood == 2]))
+#print("3-> ", len(data[data.mood == 3]))
 
 stemmer = SnowballStemmer('english')
 words = stopwords.words("english")
 
 data['cleaned'] = data['lyrics'].apply(lambda x: " ".join([stemmer.stem(i) for i in re.sub("[^a-zA-Z]", " ", x).split() if i not in words]).lower())
 
-X_train, X_test, y_train, y_test = train_test_split(data['cleaned'], data.mood, test_size=0.2 ,random_state=21)
+X_train, X_test, y_train, y_test = train_test_split(data['cleaned'], data.mood, test_size=0.2 ,random_state=99)
 
 pipeline = Pipeline([('vect', TfidfVectorizer(ngram_range=(1, 2), stop_words="english", sublinear_tf=True)),
                      ('chi',  SelectKBest(chi2, k=10000)),
@@ -42,40 +66,115 @@ feature_names = [feature_names[i] for i in chi.get_support(indices=True)]
 feature_names = np.asarray(feature_names)
 
 target_names = ['0', '1', '2', '3']
-print("top 20 keywords per mood:")
+#print("top 50 keywords per mood:")
 for i, label in enumerate(target_names):
-    top10 = np.argsort(clf.coef_[i])[-20:]
-    print("%s: %s" % (label, " ".join(feature_names[top10])))
-
-print("accuracy score: " + str(model.score(X_test, y_test)))
-print("accuracy score: " + str(model.score(X_train, y_train)))
+    top10 = np.argsort(clf.coef_[i])[-50:]
+    #print("%s: %s" % (label, " ".join(feature_names[top10])))
+    print("%s----> "  % (label))
+    wcloud(str(" ".join(feature_names[top10])))
+pred=model.predict(X_test)
+print("Testing accuracy score: " + str(model.score(X_test, y_test)))
+#print("traing accuracy score: " + str(model.score(X_train, y_train)))
+print("-----------------------------------------------------")
+print("confusion matrix")
+cm = confusion_matrix(y_test, pred)
+print(cm)
+plt.matshow(cm)
+plt.title('Confusion matrix of the classifier')
+plt.colorbar()
+plt.show()
 print("----------------------------------------------------------------------------")
+from tkinter import *
+def predict_song(aname, titlen, model):
+    #aname = input('Artist name-> ')
+    #titlen = input('title-> ')
+    if aname and titlen:
+        try:
+            inly_ly = PyLyrics.getLyrics(aname, titlen)
+        except:
+            inly_ly = ''
 
-aname = input('Artist name-> ')
-titlen = input('title-> ')
-if aname and titlen:
-    try:
-        inly_ly = PyLyrics.getLyrics(aname, titlen)
-    except:
-        inly_ly = ''
+        if inly_ly:
+            #print(inly_ly)
+            inly = inly_ly.replace('\n', ' ')
+            # print(inly)
+            decoded_class=model.predict([inly])
+            confi=model.predict_proba([inly])
+            print("\n-------------------------------------------------------------------------------------------\n")
+            ha=confi[0][0]*100
+            print("happy %->", ha)
+            an=confi[0][1]*100
+            print("angry %->", an)
+            sa=confi[0][2]*100
+            print("sad %->", sa)
+            rel=confi[0][3]*100
+            print("relax %->", rel)
+            # Pie chart, where the slices will be ordered and plotted counter-clockwise:
+            labels = 'Happy', 'Angry', 'Sad', 'Relax'
+            sizes = [ha, an, sa, rel]
+            elevate=sizes.index(max(sizes))
+            explode = (0, 0, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+            explode=list(explode)
+            explode[elevate]=0.1
+            explode=tuple(explode)
+            fig1, ax1 = plt.subplots()
+            ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+                    shadow=True, startangle=90)
+            ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
-    if inly_ly:
-        inly = inly_ly.replace('\n', ' ')
-        # print(inly)
-        decoded_class=model.predict([inly])
-        #print(decoded_class)
-        print("\n--------------------------------------------------------------------------------------------\n")
-        if str(decoded_class) == "[0]":
-            print("class-> happy ")
-        elif str(decoded_class) == "[1]":
-            print("class-> angry ")
-        elif str(decoded_class) == "[2]":
-            print("class-> sad ")
-        elif str(decoded_class) == "[3]":
-            print("class-> relax ")
+            plt.show()
+
+            #print(decoded_class)
+            print("\n--------------------------------------------------------------------------------------------\n")
+            if str(decoded_class) == "[0]":
+                print("class-> happy ")
+                res.configure(text="Happy")
+            elif str(decoded_class) == "[1]":
+                print("class-> angry ")
+                res.configure(text="Angry")
+            elif str(decoded_class) == "[2]":
+                print("class-> sad ")
+                res.configure(text="Sad")
+            elif str(decoded_class) == "[3]":
+                print("class-> relax ")
+                res.configure(text="Relax")
+            else:
+                print("failure state")
+                res.configure(text="Failure state")
+            fr = {'search_query': str(titlen) + " " + str(aname)}
+            encoded = urllib.parse.urlencode(fr)
+            print("\n youtube---> https://www.youtube.com/results?" + str(encoded))
+            print("\n")
         else:
-            print("failure state")
+            print("incorrect data or no lyrics found")
+            res.configure(text=" No Music meta found")
     else:
-        print("incorrect data or no lyrics found")
-else:
-    print("blank data")
+        print("blank data")
+        res.configure(text=" Empty ")
+    #theta=int(input("press 1 to try next song--> "))
+def show_entry_fields():
+    #print("First Name: %s\nLast Name: %s" % (e1.get(), e2.get()))
+    predict_song(e1.get(), e2.get(),model)
+
+
+master = Tk()
+Label(master, text="Artist Name").grid(row=0, pady=10)
+Label(master, text="Song Title").grid(row=1, pady=10)
+
+e1 = Entry(master)
+e2 = Entry(master)
+
+
+e1.grid(row=0, column=1)
+e2.grid(row=1, column=1)
+
+Button(master, text='Quit', command=master.quit).grid(row=3, column=0, sticky=W, pady=20)
+Button(master, text='Classify', command=show_entry_fields).grid(row=3, column=1, sticky=W, pady=20)
+
+Label(master, text="------ Mood Classifier Lyrical ------" ,bg="yellow", fg="black").grid(row=5, pady=10)
+res = Label(master ,fg="blue")
+res.grid(row=6 ,pady=20)
+mainloop()
+
+
+print("program over--------------------------------------------------------------")
